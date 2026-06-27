84547fb332113f8431d34330797102431eb7e302
Manas Rajeep <136337947+manasrajdeep@users.noreply.github.com>
2026-06-27 14:48:23 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums):
+        n = len(nums)
+        while True:
+            candidate = random.choice(nums)
+            if nums.count(candidate) > n // 2:
+                return candidate
